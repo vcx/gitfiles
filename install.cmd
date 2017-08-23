7c8d9f7cd512adbaf9881d72202fa350088eefa3
Vinicius Canto Xavier <vixavier@microsoft.com>
2017-08-23 14:42:30 -0300

    Fix typo in gitconfig and install.cmd

--- install.cmd
+++ install.cmd
@@ -1,2 +1,2 @@
-copy %userprofile\gitfiles\.gitconfig %userprofile%\.gitconfig
+copy %userprofile%\gitfiles\.gitconfig %userprofile%\.gitconfig
 git config --global --add include.path ~/gitfiles/gitconfig-windows
